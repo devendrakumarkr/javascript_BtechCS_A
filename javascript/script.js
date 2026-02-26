e525e4bc7c52458ebf173da7a7213d58395ed2bb
Devendragupta96 <devendragupta1302@gmail.com>
2026-02-26 11:50:27 +0530

    feat:assignment 2 code

--- javascript/script.js
+++ javascript/script.js
@@ -491,3 +491,86 @@
 //     await fetch("http://127.0.0.1:5502/javascript/index.html")
 // }
 // fetchData()
+
+// function* generate(){
+//     for (let index = 1; index <=4; index++) {
+//        yield index
+//     }
+// }
+
+// const gen=generate()
+// console.log(gen)
+// console.log(gen.next())
+// console.log(gen.next())
+// console.log(gen.next())
+// console.log(gen.next())
+
+
+// function add(a,b,c){
+//     return a+b+c
+// }
+
+// function add(a){
+//     return function(b){
+//         return function(c){
+//             return a+b+c
+//         }
+//     }
+// }
+
+// const first=add(1)
+// console.log(first)
+// const second=first(2)
+// console.log(second)
+// const third=second(3)
+// console.log(third)
+
+// console.log(add(1)(2)(3))
+const API_KEY="0133cc5316757ac730cc46ae342334e4"
+const form=document.querySelector("#form")
+const weatherDetail=document.querySelector(".info")
+const searchHistory=document.querySelector(".historyBtn")
+
+let cityHistory=[]
+
+form.addEventListener('submit',async function(event){
+    event.preventDefault()
+    const searchCity=city.value
+    console.log(searchCity)
+
+    if(searchCity){
+        try{
+            const res=await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${searchCity}&appid=${API_KEY}`)
+            const data=await res.json()
+            if(data.cod===200){
+                weatherDetail.innerHTML=`
+                <p>City: ${data.name}</p>
+                <p>Temp: ${(data.main.temp-273).toFixed(1)}C</p>
+                <p>Weather: ${data.weather[0].main}</p>
+                <p>Humidity: ${data.main.humidity}</p>
+                <p>Wind: ${data.wind.speed}m/s</p>`
+                cityHistory.push(searchCity)
+                localStorage.setItem("cityHistory",JSON.stringify([...new Set(cityHistory)]))
+                displayHistory()
+            }else{
+                weatherDetail.innerHTML=`<p>City not found</p>`
+            }
+        }catch(e){
+            console.log(e)
+        }
+    }
+})
+
+function displayHistory(){
+    searchHistory.innerHTML=""
+    const history=JSON.parse(localStorage.getItem("cityHistory"))
+    console.log(history)
+    if(history){
+        history.forEach((city)=>{
+            const btn=document.createElement("button")
+            btn.innerText=city
+            searchHistory.appendChild(btn)
+        })
+    }
+}
+displayHistory()
